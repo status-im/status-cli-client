0bf0e1a130a7d435f8507f534d899bc4b563a190
Jakub Sokołowski <jakub@status.im>
2019-08-30 13:02:48 -0400

    add CLI flags for parametrizing sending
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"os"
 	"fmt"
 	"time"
+	"flag"
 	"context"
+	"strings"
 	"database/sql"
 	"crypto/ecdsa"
 	"github.com/pkg/errors"
@@ -15,14 +17,6 @@ import (
 	params "github.com/status-im/status-go/params"
 	gonode "github.com/status-im/status-go/node"
 )
-
-// just some random key I generated from status-console-client
-// TODO generat key on the fly or take it from cli argumens
-const keyHex string = "4f7db2c72e3dd604b2be4258680844f1b66c911ab13701f5f33f5f5c03103221"
-const fleet string = params.FleetBeta
-const dataDir string = "/tmp/status-cli-client"
-const listenAddr string = ":30303"
-const chatName string = "whatever"
 
 func exitErr(err error) {
 	fmt.Println(err)
@@ -44,15 +38,33 @@ func (k keysGetter) PrivateKey() (*ecdsa.PrivateKey, error) {
 	return k.privateKey, nil
 }
 
+// flag variables
+var port int
+var addr, chatName, keyHex, dataDir, message string
+// randomly generated private key for tests
+var defaultKey string = "0x4f7db2c72e3dd604b2be4258680844f1b66c911ab13701f5f33f5f5c03103221"
+
+func flagsInit() {
+	flag.IntVar(&port, "port", 30303, "Listening port for Whisper node thread.")
+	flag.StringVar(&addr, "addr", "0.0.0.0", "Listening address for Whisper node thread.")
+	flag.StringVar(&chatName, "chat", "whatever", "Name of public chat to send to.")
+	flag.StringVar(&keyHex, "key", defaultKey, "Hex private key for your Status identity.")
+	flag.StringVar(&dataDir, "data", "/tmp/status-cli-client", "Location for Status data.")
+	flag.StringVar(&message, "message", "TEST", "Message to send to the public channel.")
+	flag.Parse()
+}
+
 func main() {
+	flagsInit()
+
 	var configFiles []string
 	// create a new status-go config
 	config, err := params.NewNodeConfigWithDefaultsAndFiles(
 		dataDir,
 		params.MainNetworkID,
 		[]params.Option{
-			params.WithFleet(fleet),
-			withListenAddr(listenAddr),
+			params.WithFleet(params.FleetBeta),
+			withListenAddr(fmt.Sprintf("%s:%d", addr, port)),
 		},
 		configFiles,
 	)
@@ -75,7 +87,7 @@ func main() {
 
 	var instID string = uuid.New().String()
 
-	privateKey, err := crypto.HexToECDSA(keyHex)
+	privateKey, err := crypto.HexToECDSA(strings.TrimPrefix(keyHex, "0x"))
 	if err != nil {
 		exitErr(err)
 	}
@@ -98,11 +110,10 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	// TODO parametrize the message to be sent
-	data := []byte("THIS IS A TEST MESSAGE")
+	payload := []byte(message)
 	chat := status.Chat{ID: chatName, Name: chatName}
 	// TODO error handling
-	messenger.Send(ctx, chat, data)
+	messenger.Send(ctx, chat, payload)
 	// FIXME this is a hack, wait for delivery event properly
 	time.Sleep(1 * time.Second)
 }
